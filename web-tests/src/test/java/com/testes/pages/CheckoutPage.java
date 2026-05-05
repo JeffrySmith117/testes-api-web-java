06d4f188848ea5ae0de560a352463691ab5914f4
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-05 19:36:48 -0300

    fix: adiciona espera explicita no CheckoutPage

--- web-tests/src/test/java/com/testes/pages/CheckoutPage.java
+++ web-tests/src/test/java/com/testes/pages/CheckoutPage.java
@@ -2,11 +2,15 @@ package com.testes.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 // Representa a página de checkout do SauceDemo
 public class CheckoutPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private By campoNome = By.id("first-name");
     private By campoSobrenome = By.id("last-name");
@@ -17,9 +21,11 @@ public class CheckoutPage {
 
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void preencherDados(String nome, String sobrenome, String cep) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(campoNome));
         driver.findElement(campoNome).sendKeys(nome);
         driver.findElement(campoSobrenome).sendKeys(sobrenome);
         driver.findElement(campoCep).sendKeys(cep);
@@ -27,10 +33,12 @@ public class CheckoutPage {
     }
 
     public void finalizarCompra() {
+        wait.until(ExpectedConditions.elementToBeClickable(botaoFinalizar));
         driver.findElement(botaoFinalizar).click();
     }
 
     public String obterMensagemSucesso() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(mensagemSucesso));
         return driver.findElement(mensagemSucesso).getText();
     }
 }
