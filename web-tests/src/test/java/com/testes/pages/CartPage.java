b148a361c10b5dfd4013710c2558afcb0422f7b5
JeffrySmith117 <smithjs9225@gmail.com>
2026-05-05 20:02:56 -0300

    fix: adiciona esperas explicitas no InventoryPage e CartPage

--- web-tests/src/test/java/com/testes/pages/CartPage.java
+++ web-tests/src/test/java/com/testes/pages/CartPage.java
@@ -2,23 +2,31 @@ package com.testes.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 // Representa a página do carrinho do SauceDemo
 public class CartPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private By botaoCheckout = By.id("checkout");
+    private By itensCarrinho = By.className("cart_item");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public int obterQuantidadeItens() {
-        return driver.findElements(By.className("cart_item")).size();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(itensCarrinho));
+        return driver.findElements(itensCarrinho).size();
     }
 
     public void irParaCheckout() {
+        wait.until(ExpectedConditions.elementToBeClickable(botaoCheckout));
         driver.findElement(botaoCheckout).click();
     }
 }
--- web-tests/src/test/java/com/testes/pages/InventoryPage.java
+++ web-tests/src/test/java/com/testes/pages/InventoryPage.java
@@ -2,28 +2,37 @@ package com.testes.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 // Representa a página de produtos do SauceDemo
 public class InventoryPage {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private By botaoAdicionarPrimeiroProduto = By.id("add-to-cart-sauce-labs-backpack");
     private By iconeCarrinho = By.className("shopping_cart_link");
+    private By tituloPagina = By.className("title");
 
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public String obterTituloPagina() {
-        return driver.findElement(By.className("title")).getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(tituloPagina));
+        return driver.findElement(tituloPagina).getText();
     }
 
     public void adicionarProdutoAoCarrinho() {
+        wait.until(ExpectedConditions.elementToBeClickable(botaoAdicionarPrimeiroProduto));
         driver.findElement(botaoAdicionarPrimeiroProduto).click();
     }
 
     public void irParaCarrinho() {
+        wait.until(ExpectedConditions.elementToBeClickable(iconeCarrinho));
         driver.findElement(iconeCarrinho).click();
     }
 }
